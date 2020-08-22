7bd48c17206f0021080c7b8ede37ee386c44c4f4
iambami <titatinuks@gmail.com>
2020-08-22 20:35:18 +0100

    updated commit

--- ablog/settings.py
+++ ablog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#-bdxo@)7z*o2cd==u#yf04-+nyt-!ccno2(yt7@__)($ua)68'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
